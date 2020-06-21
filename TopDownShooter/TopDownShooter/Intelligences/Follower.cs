6b191eb240a1a1f0bb249bf61fed95d7114ff0c4
Jake Congdon <Mystiick@outlook.com>
2020-06-21 16:17:00 -0500

    Rotate Follower entity to face player if they can see them

--- TopDownShooter/TopDownShooter/Intelligences/Follower.cs
+++ TopDownShooter/TopDownShooter/Intelligences/Follower.cs
@@ -22,61 +22,53 @@ namespace TopDownShooter.Intelligences
 
             if (v != null)
             {
-                // Only move the entity if it is over 50 units away from the player
-                if (distanceToPlayer > 50)
-                {
-                    MoveTowardPlayer(v, allEntities);
-                }
-                else
-                {
-                    v.Speed = 0;
-                }
+                // Reset here, because it will get set any time the entity needs to move
+                v.Speed = 0;
 
-                // If the player is closer than 100 units, shoot at them
-                if (distanceToPlayer < 100)
+                if (CanSeePlayer(allEntities))
                 {
-                    ShootAtPlayer();
+                    // Rotate entity to face player
+                    CurrentEntity.Transform.Rotation = Helpers.DetermineAngle(CurrentEntity.Transform.Position, PlayerEntity.Transform.Position);
+
+                    // Only move the entity if it is over 50 units away from the player
+                    if (distanceToPlayer > 50)
+                    {
+                        MoveTowardPlayer(v);
+                    }
+
+                    // If the player is closer than 100 units, shoot at them
+                    if (distanceToPlayer < 100)
+                    {
+                        ShootAtPlayer();
+                    }
                 }
             }
         }
 
-        private void MoveTowardPlayer(Velocity v, List<Entity> allEntities)
+        private void MoveTowardPlayer(Velocity v)
         {
-            if (CanSeePlayer(allEntities))
-            {
-                // Find my current Tile
-                Point p = new Point(
-                    (int)(CurrentEntity.Transform.Position.X / Grid.TileWidth),
-                    (int)(CurrentEntity.Transform.Position.Y / Grid.TileHeight)
-                );
-                Tile myTile = Grid.Tiles[p.X, p.Y];
-                Tile targetTile = myTile;
+            // Find my current Tile
+            Point p = new Point(
+                (int)(CurrentEntity.Transform.Position.X / Grid.TileWidth),
+                (int)(CurrentEntity.Transform.Position.Y / Grid.TileHeight)
+            );
+            Tile myTile = Grid.Tiles[p.X, p.Y];
+            Tile targetTile = myTile;
 
-                foreach (Tile t in myTile.Neighbors)
+            foreach (Tile t in myTile.Neighbors)
+            {
+                if (t.DistanceToPlayer < targetTile.DistanceToPlayer && t.CanTravelThrough)
                 {
-                    if (t.DistanceToPlayer < targetTile.DistanceToPlayer && t.CanTravelThrough)
-                    {
-                        targetTile = t;
-                    }
+                    targetTile = t;
                 }
-
-                // Adding half the tile width,height to the X,Y coords lets us line up the center of the sprite with the center of the tile
-                Vector2 targetPosition = new Vector2(targetTile.Location.X * Grid.TileWidth + (Grid.TileWidth / 2), targetTile.Location.Y * Grid.TileHeight + (Grid.TileHeight / 2));
-
-                // Need to subtract origin here, otherwise it tries to line up the top left corner with the center of the tile, causing it to get stuck on 1x1 gaps or corners
-                v.Direction = targetPosition - CurrentEntity.Transform.Position - CurrentEntity.Sprite.Origin;
-                v.Speed = 50;
-
             }
-            else if (1 == 2 /*lastKnownPosition != Vector2.Zero*/)
-            {
-                // TODO: Move to last known location
-                // Need to calculate route for this, instead of just navigating tiles
-            }
-            else
-            {
-                v.Speed = 0;
-            }
+
+            // Adding half the tile width,height to the X,Y coords lets us line up the center of the sprite with the center of the tile
+            Vector2 targetPosition = new Vector2(targetTile.Location.X * Grid.TileWidth + (Grid.TileWidth / 2), targetTile.Location.Y * Grid.TileHeight + (Grid.TileHeight / 2));
+
+            // Need to subtract origin here, otherwise it tries to line up the top left corner with the center of the tile, causing it to get stuck on 1x1 gaps or corners
+            v.Direction = targetPosition - CurrentEntity.Transform.Position - CurrentEntity.Sprite.Origin;
+            v.Speed = 50;
         }
 
         private bool CanSeePlayer(List<Entity> allEntities)
