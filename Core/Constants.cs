1330c33d1a35643c85650dbf1160a420835fb893
Mystiick <Mystiick@outlook.com>
2022-07-23 23:04:55 -0500

    Move core files into core project folder. Needed to do this in two steps to retain history

---
+++ Core/Constants.cs
@@ -0,0 +1,12 @@
+﻿namespace MystiickCore;
+
+public class Constants
+{
+    public struct GameEvent
+    {
+        public const string EnemyKilled = "GameEvent.EnemyKilled";
+        public const string PlayerKilled = "GameEvent.PlayerKilled";
+        public const string MapGridReset = "GameEvent.MapGridReset";
+        public const string SetupWorld = "LoadMap.SetupWorld";
+    }
+}
---
+++ Core/Enums.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MystiickCore;
+
+// TODO: Probably can just get rid of this and use constants for messages types
+public enum EventType
+{
+    Test,
+    UserInterface,
+    Score,
+    LoadMap,
+    Spawn,
+    GameEvent
+}
+
+public enum MouseAndKeys
+{
+    LeftClick,
+    RightClick,
+    ScrollClick,
+    ScrollUp,
+    ScrollDown,
+    A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z,
+    Space,
+    Alt,
+    Shift,
+    Tab,
+    One,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Zero,
+    Tilde
+}
+
+// TODO: Is there a way to get rid of this
+public enum EntityType
+{
+    Unassigned,
+    Player,
+    Wall,
+    Enemy,
+    Pickup,
+    TileGrid,
+}
