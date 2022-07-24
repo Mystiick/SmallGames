82efc2057b5f72b350c09458fc875cd058022a02
Mystiick <Mystiick@outlook.com>
2022-07-23 23:33:42 -0500

    Add files that were lost to the void

--- Core/Models/BaseStage.cs
+++ Core/Models/BaseStage.cs
@@ -1,40 +1,98 @@
 ﻿using System;
-using System.IO;
 
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+using MonoGame.Extended;
 using MonoGame.Extended.Gui;
-using MonoGame.Extended.Gui.Markup;
+
+using MystiickCore.Interfaces;
+using MystiickCore.Managers;
+using MystiickCore.Services;
 
 namespace MystiickCore.Models;
 
-public abstract class BaseScreen
+public abstract class BaseStage : IDisposable
 {
-    public Screen Screen
+    protected GraphicsDevice GraphicsDevice;
+    protected SpriteBatch SpriteBatch;
+    protected ContentCacheManager ContentCache;
+    protected StageManager StageManager;
+    protected GuiSystem UserInterface;
+    protected EntityComponentManager EntityComponentManager;
+    protected IInputManager InputManager;
+
+    public OrthographicCamera Camera { get; protected set; }
+    public readonly Guid StageID;
+
+    protected BaseStage()
     {
-        get;
-        private set;
+        StageID = Guid.NewGuid();
     }
 
-    public BaseScreen(string markupPath)
+    public virtual void InitializeBase(
+        SpriteBatch spriteBatch,
+        StageManager stageManager,
+        GuiSystem gui,
+        IInputManager input,
+        EntityComponentManager ecm,
+        object[]? args)
     {
-        LoadFromMarkup(markupPath);
+        GraphicsDevice = spriteBatch.GraphicsDevice;
+        SpriteBatch = spriteBatch;
+        StageManager = stageManager;
+        UserInterface = gui;
+        InputManager = input;
+        EntityComponentManager = ecm;
+        EntityComponentManager.Init();
+        Camera = new OrthographicCamera(GraphicsDevice) { Zoom = 3f };
     }
 
-    private void LoadFromMarkup(string markupPath)
+    /// <summary>
+    /// Function that declares that this stage has the focus
+    /// </summary>
+    public virtual void Start()
     {
-        var parser = new MarkupParser();
-        this.Screen = new Screen()
+
+    }
+
+    public virtual void LoadContent(ContentCacheManager contentManager)
+    {
+        ContentCache = contentManager;
+    }
+
+    public virtual void Update(GameTime gameTime)
+    {
+        EntityComponentManager.Update(gameTime);
+    }
+
+    public virtual void Draw()
+    {
+        EntityComponentManager.Draw(SpriteBatch, GraphicsDevice, Camera);
+    }
+
+    #region IDisposable Support
+    private bool disposedValue = false; // To detect redundant calls
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!disposedValue)
         {
-            Content = parser.Parse(
-                Path.Combine(AppContext.BaseDirectory, markupPath),
-                new object()
-            )
-        };
+            if (disposing)
+            {
+                MessagingService.UnsubscribeParent(this.StageID);
+            }
 
-        SetupEvents();
+            disposedValue = true;
+        }
     }
 
-    protected virtual void SetupEvents()
+    // This code added to correctly implement the disposable pattern.
+    public void Dispose()
     {
-
+        // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+        Dispose(true);
     }
+    #endregion
+
 }
