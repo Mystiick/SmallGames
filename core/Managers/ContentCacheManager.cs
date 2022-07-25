8b882cb8d44c4a8c1c41d175ba5c6b940e51b282
Mystiick <Mystiick@outlook.com>
2022-07-25 16:59:55 -0500

    Touching ContentCacheManager.cs to get it back into Github

--- core/Managers/ContentCacheManager.cs
+++ core/Managers/ContentCacheManager.cs
@@ -3,7 +3,6 @@
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 
-using MonoGame.Extended.Gui;
 using MonoGame.Extended.TextureAtlases;
 using MonoGame.Extended.Tiled;
 
